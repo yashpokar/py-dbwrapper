be182eb7f4a78196e2c2c639ce5d64ffdf2bb73a
Yash Patel <yash.p@digitalshelfiq.com>
2018-06-30 12:10:19 +0530

    connector class designing

--- dbee/connector.py
+++ dbee/connector.py
@@ -12,7 +12,4 @@ class Connector:
             if not Connector._conn:
                 Connector._conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port, **kwargs)
         except Exception as e:
-            Connector._conn = None
-            Connector.connect(host, user, password, dbname, port, **kwargs)
-        finally:
             DatabaseConnectionError(e)
