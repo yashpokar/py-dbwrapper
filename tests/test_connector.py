9bcf94245d9c8c780b92109415c072698475e2d2
Yash Patel <yash.p@digitalshelfiq.com>
2018-06-30 12:11:04 +0530

    tests added

---
+++ tests/test_connector.py
@@ -0,0 +1,9 @@
+import unittest
+from dbee.connector import Connector
+
+
+class TestConnector(unittest.TestCase):
+
+    def test_connection(self):
+        # TODO :: Test database connector
+        pass
